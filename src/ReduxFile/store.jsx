a7c90f57b242f3b59e50f862907a55a85f2c412a
neyaznafiz <niiyazobug@gmail.com>
2022-12-11 03:16:39 +0600

    redux devtools impliment

--- src/ReduxFile/store.jsx
+++ src/ReduxFile/store.jsx
@@ -1,5 +1,9 @@
+import { composeWithDevTools } from "@redux-devtools/extension";
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import { rootReducer } from "./Reducer/rootReducer";
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
